04b69cf97ec8af06309741a1d26a5aa991f06565
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-01-05 08:20:29 +0000

    Set system.stateVersion = "22.11".

--- image.nix
+++ image.nix
@@ -1,4 +1,5 @@
 { pkgs, modulesPath, lib, ... }: {
     boot.kernelPackages = pkgs.linuxPackages_latest;
     boot.supportedFilesystems = lib.mkForce [ "vfat" "f2fs" "ntfs" ];
+    system.stateVersion = "22.11";
 }
