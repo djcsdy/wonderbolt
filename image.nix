ffa5db95e8faa586e2a756a625988de22651130a
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-01-05 09:06:49 +0000

    Base image on "minimal" NixOS profile.
    No GUI, no documentation.

--- image.nix
+++ image.nix
@@ -1,4 +1,5 @@
 { pkgs, modulesPath, lib, ... }: {
+    imports = [ "${modulesPath}/profiles/minimal.nix" ];
     boot.kernelPackages = pkgs.linuxPackages_latest;
     boot.supportedFilesystems = lib.mkForce [ "vfat" "f2fs" "ntfs" ];
     system.stateVersion = "22.11";
