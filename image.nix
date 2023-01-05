1fa8263d981b5bc2b005d004e1d54385c8cf722a
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-01-05 09:07:41 +0000

    Re-enable logrotate service.
    This is disabled by the minimal profile.

--- image.nix
+++ image.nix
@@ -2,5 +2,6 @@
     imports = [ "${modulesPath}/profiles/minimal.nix" ];
     boot.kernelPackages = pkgs.linuxPackages_latest;
     boot.supportedFilesystems = lib.mkForce [ "vfat" "f2fs" "ntfs" ];
+    services.logrotate.enable = true;
     system.stateVersion = "22.11";
 }
