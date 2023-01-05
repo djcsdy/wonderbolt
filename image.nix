6beef225ba06dca19f36f82853ad1aebc1514c0e
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-01-05 09:18:25 +0000

    Grow root partition on boot.

--- image.nix
+++ image.nix
@@ -1,6 +1,7 @@
 { pkgs, modulesPath, lib, ... }: {
     imports = [ "${modulesPath}/profiles/minimal.nix" ];
     boot.cleanTmpDir = true;
+    boot.growPartition = true;
     boot.kernelPackages = pkgs.linuxPackages_latest;
     boot.supportedFilesystems = lib.mkForce [ "vfat" "f2fs" "ntfs" ];
     services.logrotate.enable = true;
