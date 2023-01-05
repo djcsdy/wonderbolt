b879045fefac3a323b55b654faa39abd64e1f5dd
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-01-05 10:19:38 +0000

    Set default locale to en_GB.UTF-8.

--- image.nix
+++ image.nix
@@ -4,6 +4,7 @@
     boot.kernelPackages = pkgs.linuxPackages_latest;
     boot.supportedFilesystems = lib.mkForce [ "vfat" "f2fs" "ntfs" ];
     boot.tmpOnTmpfs = true;
+    i18n.defaultLocale = "en_GB.UTF-8";
     services.logrotate.enable = true;
     system.stateVersion = "22.11";
 }
