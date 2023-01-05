5f1f224f8cf08d8f1951f91f536fcdcfed04caa2
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-01-05 09:55:18 +0000

    Use tmpfs for /tmp.

--- image.nix
+++ image.nix
@@ -1,9 +1,9 @@
 { pkgs, modulesPath, lib, ... }: {
     imports = [ "${modulesPath}/profiles/minimal.nix" ];
-    boot.cleanTmpDir = true;
     boot.growPartition = true;
     boot.kernelPackages = pkgs.linuxPackages_latest;
     boot.supportedFilesystems = lib.mkForce [ "vfat" "f2fs" "ntfs" ];
+    boot.tmpOnTmpfs = true;
     services.logrotate.enable = true;
     system.stateVersion = "22.11";
 }
